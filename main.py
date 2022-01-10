146cd6e3193e5d84ae56cfadc263b6155218b9d5
Amine GABSI <mamine.gabsi@gmail.com>
2022-01-10 21:18:25 +0100

    addColorChange

--- main.py
+++ main.py
@@ -6,3 +6,33 @@ width, height = img.size
 def getPixelColor(image, x, y):
     r,g,b = image.getpixel((x, y))
     print("R: {}, G: {}, B: {}".format(r,g,b))
+
+def changeToNegative(image):
+    for y in range(height):
+        for x in range(width):
+            r,g,b = image.getpixel((x, y))
+            image.putpixel((x, y), (255-r,255-g,255-b))
+    image.show()
+
+def makeItGray(image):
+    for y in range(height):
+        for x in range(width):
+            r,g,b = image.getpixel((x, y))
+            grayColor = (r+g+b)//3
+            image.putpixel((x, y), (grayColor, grayColor, grayColor))
+    image.show()
+
+def halfNegativeHalfGray(image):
+    width1, height1 = image.size
+    imgHalf = height1//2
+    for y in range(height):
+        for x in range(width):
+            r,g,b = image.getpixel((x, y))
+            if y < imgHalf:
+                image.putpixel((x, y), (255-r, 255-g, 255-b))
+            else:
+                grayColor = (r+g+b)//3
+                image.putpixel((x, y), (grayColor, grayColor, grayColor))
+    image.show()
+
+halfNegativeHalfGray(img)
